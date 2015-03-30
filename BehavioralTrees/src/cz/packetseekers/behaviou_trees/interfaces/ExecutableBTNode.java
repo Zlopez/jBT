7a729ebdc930a74eed5baf3a08c4f1f620e6a483
Zlopez <michalkonec666@gmail.com>
2015-03-30 13:28:32 +0200

    Added example code to comments
    Added more detailed example in comments of interface

--- BehavioralTrees/src/cz/packetseekers/behaviou_trees/interfaces/ExecutableBTNode.java
+++ BehavioralTrees/src/cz/packetseekers/behaviou_trees/interfaces/ExecutableBTNode.java
@@ -14,7 +14,25 @@ public interface ExecutableBTNode {
 	/**
 	 * Execute method for node. It must implement the way node works and set
 	 * result variable. Status change of {@link BTNode} must be done manually by
-	 * developer in this method.
+	 * developer in this method. Example:
+	 * 
+	 * <pre>
+	 * {@code 
+	 * public void execute(Object object) {
+	 * 	this.changeStatus(Status.IN_PROGRESS);
+	 * 
+	 * 	Boolean result = false;
+	 * 
+	 * 	this.changeStatus(Status.WAITING);
+	 * 
+	 * 	for (BTNode child : this.getChildren()) {
+	 * 		((ExecutableBTNode) child).execute(object);
+	 * 		result = !child.getResult();
+	 * 	}
+	 * 	this.setResult(result);
+	 * 	this.changeStatus(Status.DONE);
+	 * }}
+	 * </pre>
 	 * 
 	 * @param object
 	 *            Optional object to work with, it may contains everything
